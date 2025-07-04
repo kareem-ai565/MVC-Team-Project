a2008a6397c375991b711d4d9a5d4eecad2df2a4
kareem-ai565 <130701387+kareem-ai565@users.noreply.github.com>
2025-07-04 16:53:09 +0300

    doctors from admin

--- MVC-Team Project/Repositories/Interfaces/IDoctorRepository.cs
+++
@@ -1,1 +0,0 @@
-// IDoctorRepository.cs placeholder
---
+++ MVC-Team Project/View Models/DoctorAvailabilityVM.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC_Team_Project.View_Models
+{
+    public class DoctorAvailabilityVM
+    {
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public int SlotDuration { get; set; }
+        public int MaxPatients { get; set; }
+    }
+
+}
+
