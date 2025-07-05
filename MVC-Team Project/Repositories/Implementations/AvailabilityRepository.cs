46b36af81896524006cf50ea6b12ac0f2614213b
fae101 <faegobran@gmail.com>
2025-07-05 17:39:55 +0300

    availability repos fix

--- MVC-Team Project/Repositories/Implementations/AvailabilityRepository.cs
+++ MVC-Team Project/Repositories/Implementations/AvailabilityRepository.cs
@@ -4,11 +4,7 @@ using MVC_Team_Project.Repositories.Interfaces;
 
 namespace MVC_Team_Project.Repositories.Implementations
 {
-<<<<<<< HEAD
-    public class AvailabilityRepository: IAvailabilityRepository
-=======
     public class AvailabilityRepository : IAvailabilityRepository
->>>>>>> 792ebac25cff0b5c4e8142fb38a771d72712746f
     {
         private readonly ClinicSystemContext _context;
 
@@ -17,44 +13,6 @@ namespace MVC_Team_Project.Repositories.Implementations
             _context = context;
         }
 
-<<<<<<< HEAD
-        public async Task<IEnumerable<Availability>> GetAllAsync() =>
-            await _context.Availabilities.Include(a => a.Doctor).ToListAsync();
-
-        public async Task<Availability?> GetByIdAsync(int id) =>
-            await _context.Availabilities.FindAsync(id);
-
-        public async Task AddAsync(Availability entity) =>
-            await _context.Availabilities.AddAsync(entity);
-
-        public void Update(Availability entity) =>
-            _context.Availabilities.Update(entity);
-
-        public void Delete(Availability entity) =>
-            _context.Availabilities.Remove(entity);
-
-        public async Task SaveChangesAsync() =>
-            await _context.SaveChangesAsync();
-
-        public async Task<IEnumerable<Availability>> GetByDoctorIdAsync(int doctorId) =>
-            await _context.Availabilities
-                .Where(a => a.DoctorId == doctorId)
-                .ToListAsync();
-
-        public async Task<IEnumerable<Availability>> GetByDateAsync(DateTime date) =>
-            await _context.Availabilities
-                .Where(a => a.AvailableDate == date)
-                .ToListAsync();
-
-        public async Task<bool> IsSlotTakenAsync(int doctorId, DateTime date, TimeSpan startTime) =>
-            await _context.Availabilities.AnyAsync(a =>
-                a.DoctorId == doctorId &&
-                a.AvailableDate == date &&
-                a.StartTime == startTime);
-    }
-}
-
-=======
         public async Task<IEnumerable<Availability>> GetAllAsync()
         {
             return await _context.Availabilities
@@ -99,4 +57,3 @@ namespace MVC_Team_Project.Repositories.Implementations
         }
     }
 }
->>>>>>> 792ebac25cff0b5c4e8142fb38a771d72712746f
--- MVC-Team Project/Repositories/Interfaces/IAvailabilityRepository.cs
+++ MVC-Team Project/Repositories/Interfaces/IAvailabilityRepository.cs
@@ -2,17 +2,8 @@
 
 namespace MVC_Team_Project.Repositories.Interfaces
 {
-<<<<<<< HEAD
-    public interface IAvailabilityRepository: IRepository<Availability>
-    {
-        Task<IEnumerable<Availability>> GetByDoctorIdAsync(int doctorId);
-        Task<IEnumerable<Availability>> GetByDateAsync(DateTime date);
-        Task<bool> IsSlotTakenAsync(int doctorId, DateTime date, TimeSpan startTime);
-
-=======
     public interface IAvailabilityRepository : IRepository<Availability>
     {
         Task<IEnumerable<Availability>> GetByDoctorIdAsync(int doctorId);
->>>>>>> 792ebac25cff0b5c4e8142fb38a771d72712746f
     }
 }
