953a8422b075307637b3a13adda3b23f0629df29
kareem-ai565 <130701387+kareem-ai565@users.noreply.github.com>
2025-07-05 20:21:25 +0300

    specialities cards and doctors for user and edits in layout

---
+++ MVC-Team Project/View Models/SpecialtyForShowVM.cs
@@ -0,0 +1,16 @@
+﻿using MVC_Team_Project.Models;
+
+namespace MVC_Team_Project.View_Models
+{
+    public class SpecialtyForShowVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // List of doctors in that specialty
+        public List<DoctorsVM> Doctors { get; set; }
+    }
+}
